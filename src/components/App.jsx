32752d81dd742432c313a83cc8ede35d4ff2e036
Marlena Madej <madejmarlena@gmail.com>
2022-10-16 22:04:20 +0200

    prop-Types

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
-export const App = () => {
+import PropTypes from 'prop-types';
+
+export const App = ({ children }) => {
   return (
     <div
       style={{
-        height: '100vh',
+        height: '100%',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
+        padding: '50px 10px',
       }}
     >
-      React homework template
+      {children}
     </div>
   );
+};
+
+App.propTypes = {
+  children: PropTypes.array.isRequired,
 };

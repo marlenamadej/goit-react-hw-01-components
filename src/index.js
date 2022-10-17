6ef2037d10fa3145552d15c9da17aa473450f241
Marlena Madej <madejmarlena@gmail.com>
2022-10-17 18:41:49 +0200

    zadania

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 import user from './components/zad1Profile/user.json';
-// import data from './components/zad2Statistic/data.json';
-// import friends from './components/zad3Friends/friends.json';
-// import transactions from './components/zad4Transaction/transactions.json';
+import data from './components/zad2Statistic/data.json';
+import friends from './components/zad3Friends/friends.json';
+import transactions from './components/zad4Transaction/transactions.json';
 
 import { App } from 'components/App';
 import { Profile } from './components/zad1Profile/Profile';
+import { Statistics } from './components/zad2Statistic/Statistic';
+import { FriendList } from './components/zad3Friends/FriendsList';
+import { TransactionHistory } from './components/zad4Transaction/Transaction';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -20,6 +23,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         avatar={user.avatar}
         stats={user.stats}
       />
+       <Statistics title="Upload stats" stats={data} />
+       <FriendList friends={friends} />
+       <TransactionHistory items={transactions} />
     </App>
   </React.StrictMode>
 );
